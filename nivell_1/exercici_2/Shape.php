46417684bdf9af89eb5a0239c7f2336c42cf9d63
Alan GIRALT <alangiralt@telefonica.net>
2026-04-22 08:09:31 +0200

    feat: force data validation via __construct and setters

--- nivell_1/exercici_2/Shape.php
+++ nivell_1/exercici_2/Shape.php
@@ -5,13 +5,24 @@ abstract class Shape {
     function __construct(
         private float $width,
         private float $height) {
+        if ($this->width <= 0) throw new InvalidArgumentException('$width is less or equal to 0');
+        if ($this->height <= 0) throw new InvalidArgumentException('$height is less or equal to 0');
     }
     // Getters
-    public function getWidth() {
+    public function getWidth(): float {
         return $this->width;
     }
-    public function getHeight() {
+    public function getHeight(): float {
         return $this->height;
+    }
+    // Setters
+    public function setWidth(float $width): void {
+        if ($width <= 0) throw new InvalidArgumentException('$width is less or equal to 0');
+        $this->width = $width;
+    }
+    public function setHeight(float $height): void {
+        if ($height <= 0) throw new InvalidArgumentException('$height is less or equal to 0');
+        $this->height = $height;
     }
     //Methods
     public abstract function calculateArea(): float;
